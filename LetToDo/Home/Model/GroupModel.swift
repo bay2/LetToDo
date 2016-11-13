65bff8bcbf5a70656f7beee619494c28f4a97162
蔡学敏 <382499488@qq.com>
2016-11-13 22:23:58 +0800

    update group title

---
+++ LetToDo/Home/Model/GroupModel.swift
@@ -0,0 +1,19 @@
+//
+//  GroupModel.swift
+//  LetToDo
+//
+//  Created by xuemincai on 2016/10/31.
+//  Copyright © 2016年 xuemincai. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class GroupModel: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
---
+++ LetToDo/Home/ViewModel/GroupViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  GroupViewModel.swift
+//  LetToDo
+//
+//  Created by xuemincai on 2016/10/31.
+//  Copyright © 2016年 xuemincai. All rights reserved.
+//
+
+import Foundation
